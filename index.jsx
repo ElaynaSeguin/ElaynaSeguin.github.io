4db4b4edb482decc25a6b7289efbb42b5a9d87eb
Elayna (Lana) <62639647+ElaynaSeguin@users.noreply.github.com>
2024-01-16 23:39:57 -0800

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="frame-wrapper">
+        <div className="frame">
+          <div className="text-wrapper">Hello!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
